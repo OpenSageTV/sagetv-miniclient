a7fe7394545461bc4341d3f487e681a572ceef14
seans <sean.stuckless@gmail.com>
2015-09-15 20:50:44 -0400

    Switched to single surface rendering

---
+++ core/src/sagex/miniclient/util/IOUtil.java
@@ -0,0 +1,41 @@
+package sagex.miniclient.util;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.ByteBuffer;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
+import java.nio.channels.WritableByteChannel;
+
+/**
+ * Created by seans on 15/09/15.
+ */
+public class IOUtil {
+    public static void fastChannelCopy(final ReadableByteChannel src, final WritableByteChannel dest) throws IOException {
+        final ByteBuffer buffer = ByteBuffer.allocateDirect(16 * 1024);
+        while (src.read(buffer) != -1) {
+            // prepare the buffer to be drained
+            buffer.flip();
+            // write to the channel, may block
+            dest.write(buffer);
+            // If partial transfer, shift remainder down
+            // If buffer is empty, same as doing clear()
+            buffer.compact();
+        }
+        // EOF will leave buffer in fill state
+        buffer.flip();
+        // make sure the buffer is fully drained.
+        while (buffer.hasRemaining()) {
+            dest.write(buffer);
+        }
+    }
+
+    public static void fastCopy(final InputStream src, final OutputStream dest) throws IOException {
+        final ReadableByteChannel inputChannel = Channels.newChannel(src);
+        final WritableByteChannel outputChannel = Channels.newChannel(dest);
+        fastChannelCopy(inputChannel, outputChannel);
+        inputChannel.close();
+        outputChannel.close();
+    }
+}
