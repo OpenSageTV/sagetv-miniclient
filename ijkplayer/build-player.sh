7bd015fe7ac18db55be5187125dfa9ce51a5ea74
seans <sean.stuckless@gmail.com>
2015-11-14 14:46:57 -0500

    Updated IJKPLayer and added scripts for rebuilding IJKPlayer

---
+++ ijkplayer/build-player.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+if [ "$ANDROID_SDK" = "" ] ; then
+    echo "Set ANDROID_SDK to be the location of your Sdk"
+    exit 1
+fi
+
+if [ ! -d Ndk ] ; then
+    echo "run init-sources to init the Ndk"
+    exit 1
+fi
+
+export ANDROID_NDK=`pwd`/Ndk/android-ndk-r10e
+#export ANDROID_SDK=/home/sls/Android/Sdk/
+
+cd ijkplayer
+./init-android.sh
+cd android/contrib/
+#./compile-ffmpeg.sh clean
+./compile-ffmpeg.sh all
+cd ..
+./compile-ijk.sh all
+cd ..
+
+echo "run copy-resources to update the project's artifacts with the new player"
---
+++ ijkplayer/init-sources.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+if [ ! -d ijkplayer ] ; then
+    echo "Fetching IJKPlayer Sources"
+    git clone https://github.com/Bilibili/ijkplayer.git
+    #cd ijkplayer
+    #git remote add upstream https://github.com/Bilibili/ijkplayer.git
+    #git fetch upstream
+    #git rebase upstream/master
+    #cd ..
+fi
+
+if [ ! -d Ndk ] ; then
+    echo "Setting up NDK"
+    mkdir Ndk
+    cd Ndk/
+    wget http://dl.google.com/android/ndk/android-ndk-r10e-linux-x86_64.bin
+    chmod 755 android-ndk-r10e-linux-x86_64.bin
+    ./android-ndk-r10e-linux-x86_64.bin
+    cd ..
+fi
+
+
+
+
