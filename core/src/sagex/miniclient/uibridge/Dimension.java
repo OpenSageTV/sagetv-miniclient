86ee24477598eeaff829f9c5e21f52b777312090
seans <sean.stuckless@gmail.com>
2016-01-23 10:50:51 -0500

    Added ability change screen resolution to half native

--- core/src/sagex/miniclient/uibridge/Dimension.java
+++ core/src/sagex/miniclient/uibridge/Dimension.java
@@ -9,6 +9,10 @@ public class Dimension {
         this.height = height;
     }
 
+    public Dimension(Dimension size) {
+        updateFrom(size);
+    }
+
     @Override
     public String toString() {
         final StringBuilder sb = new StringBuilder("Dimension{");
@@ -32,5 +36,37 @@ public class Dimension {
 
     public void setHeight(int height) {
         this.height = height;
+    }
+
+    public void updateFrom(Dimension otherDim) {
+        update(otherDim.width, otherDim.height);
+    }
+
+    public void update(int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    public boolean equals(int width, int height) {
+        return this.width == width && this.height == height;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Dimension dimension = (Dimension) o;
+
+        if (width != dimension.width) return false;
+        return height == dimension.height;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = width;
+        result = 31 * result + height;
+        return result;
     }
 }
