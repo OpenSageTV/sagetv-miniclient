a93bbe8a8b6d86cbae0a4c2228166d66cca6985f
seans <sean.stuckless@gmail.com>
2016-03-12 07:36:30 -0500

    Fixed https://github.com/OpenSageTV/sagetv-miniclient/issues/46 (View Hardware Decoders crashing on Android TV)

--- android-shared/src/main/java/sagex/miniclient/android/SettingsActivity.java
+++ android-shared/src/main/java/sagex/miniclient/android/SettingsActivity.java
@@ -1,9 +1,9 @@
 package sagex.miniclient.android;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
-public class SettingsActivity extends Activity {
+public class SettingsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
