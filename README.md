982ff66f14d64582dc7f3723325aca8fcded6d3c
Sean Stuckless <sean.stuckless@gmail.com>
2016-03-26 07:35:58 -0400

    refactoring to use a single APK

---
+++ README.md
@@ -0,0 +1,32 @@
+# SageTV MiniClient
+[ ![Download](https://api.bintray.com/packages/opensagetv/sagetv/SageTV/images/download.svg) ](https://bintray.com/opensagetv/sagetv/SageTV/_latestVersion)
+
+SageTV MiniClient is an HD Extender (ie, HD300) for Android devices. Using this app, you can connect to your SageTV server like you can with an HD200 or HD300 device. Once connected you will get your familiar SageTV UI as this App basically identifies itself as an HD300 extender.
+This client supports the playback of many different Media Formats, and, if hardware decoding is available on the device, then it will be used.
+
+## Supported Hardware
+* Android Phones/Tablets running Android 4.4 and higher
+* Android TV running Android 5.0 and higher
+* FireTV running Android 5.0 and higher
+
+## Installing
+SageTV MiniClient for Android is available on the [Google Play Store](https://play.google.com/store/apps/details?id=sagex.miniclient.android.tv&hl=en) and from [BinTray](https://bintray.com/opensagetv/sagetv/SageTV/_latestVersion).  
+If isntalling from BinTray then you'll need to know how to side load apps. 
+
+## Features
+* Runs like HD300 (ie, impersonates and HD300)
+* H264 content in MP4, MKV, and TS containers (hardware decoding)
+* H265 (HEVC) content in MKV containers (hardware decoding)
+* MPEG2 content in MPG and TS containers (hardware decoding on some platforms)
+* AVI,FLV,WEBM formats as well
+* Support for Subtitles/CC in .srt files
+* Placeshifter Mode
+
+## Limitations
+* No support for DVD/Bluray Discs
+* No Audio Pass-through for IJKPlayer
+* No Embedded Subtitle/CC support
+* No PIP
+
+## Screenshots
+![Menu](https://raw.githubusercontent.com/OpenSageTV/sagetv-miniclient/master/playstore/tv/gemstone_tv_menu.png)
--- android-shared/src/main/java/sagex/miniclient/android/SettingsFragment.java
+++ android-shared/src/main/java/sagex/miniclient/android/SettingsFragment.java
@@ -2,12 +2,15 @@ package sagex.miniclient.android;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.preference.EditTextPreference;
 import android.preference.Preference;
+import android.preference.Preference.OnPreferenceChangeListener;
 import android.preference.PreferenceFragment;
 import android.support.v7.app.AlertDialog;
 import android.text.Html;
 
 import sagex.miniclient.Version;
+import sagex.miniclient.android.util.NetUtil;
 import sagex.miniclient.prefs.PrefStore;
 import sagex.miniclient.prefs.PrefStore.Keys;
 import sagex.miniclient.util.ClientIDGenerator;
@@ -71,12 +74,40 @@ public class SettingsFragment extends PreferenceFragment {
 
             final Preference version = findPreference("version");
             version.setSummary(Version.VERSION);
-            final Preference clientid = findPreference(Keys.client_id);
-            ClientIDGenerator gen = new ClientIDGenerator();
+
+            final Preference ipaddress = findPreference("ipaddress");
+            ipaddress.setSummary(NetUtil.getIPAddress(true));
+
+            final EditTextPreference clientid = (EditTextPreference) findPreference(Keys.client_id);
+            final ClientIDGenerator gen = new ClientIDGenerator();
             if (prefs.getString(Keys.client_id) == null) {
                 prefs.setString(Keys.client_id, gen.generateId());
             }
-            clientid.setSummary(prefs.getString(Keys.client_id) + " (" + gen.id2string(prefs.getString(Keys.client_id)) + ")");
+            //clientid.setSummary(prefs.getString(Keys.client_id) + " (" + gen.id2string(prefs.getString(Keys.client_id)) + ")");
+            updateClientIDSummary(clientid, prefs.getString(Keys.client_id), gen);
+            clientid.setOnPreferenceChangeListener(new OnPreferenceChangeListener() {
+                @Override
+                public boolean onPreferenceChange(Preference preference, Object newValue) {
+                    if (newValue==null) return false;
+                    String val = (String)newValue;
+                    if (val.trim().length()==0) return false;
+                    //System.out.println("ONCHANGE: " + val);
+                    if (val.indexOf(':')<0) {
+                        //System.out.println("ONCHANGE: CONVERT TO ID BEFORE " + val);
+                        // user entered text, convert to mac address
+                        val = gen.generateId(val);
+                        //System.out.println("ONCHANGE: CONVERT TO ID AFTER " + val);
+                        prefs.setString(Keys.client_id, val);
+                        updateClientIDSummary(preference, val, gen);
+                        // return false, so that this text value doesn't get persisted.
+                        return false;
+                    } else {
+                        //System.out.println("ONCHANGE: VAL WAS ID " + val);
+                        updateClientIDSummary(preference, val, gen);
+                        return true;
+                    }
+                }
+            });
 
             final Preference decoders = findPreference("decoders");
             decoders.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
@@ -92,6 +123,10 @@ public class SettingsFragment extends PreferenceFragment {
         } catch (Throwable t) {
             t.printStackTrace();
         }
+    }
+
+    void updateClientIDSummary(Preference clientid, String value, ClientIDGenerator gen) {
+        clientid.setSummary(value + " (" + gen.id2string(value) + ")");
     }
 
     private void showHardwareDecoderInfo() {
