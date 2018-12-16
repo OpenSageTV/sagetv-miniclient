bf08026707d9f0d3bda5e28aca39d59dca909b53
Sean Stuckless <sean.stuckless@gmail.com>
2018-12-16 17:59:32 -0500

    bumped version

--- core/src/main/java/sagex/miniclient/Version.java
+++ core/src/main/java/sagex/miniclient/Version.java
@@ -4,5 +4,5 @@ package sagex.miniclient;
  * Created by seans on 30/11/15.
  */
 public class Version {
-    public static final String VERSION = "1.3.5-dev";
+    public static final String VERSION = "1.3.5";
 }
