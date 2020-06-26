14b05cf1a32255217b4f13682409b6a352f4828b
Joshua V. Lewis <josh@joshandmonique.com>
2020-06-26 08:33:09 -0400

    Merging changes

---
+++ android-shared/src/main/java/sagex/miniclient/android/video/exoplayer2/CustomMediaCodecSelector.java
@@ -0,0 +1,47 @@
+package sagex.miniclient.android.video.exoplayer2;
+
+import android.support.annotation.Nullable;
+
+import com.google.android.exoplayer2.mediacodec.MediaCodecInfo;
+import com.google.android.exoplayer2.mediacodec.MediaCodecSelector;
+import com.google.android.exoplayer2.mediacodec.MediaCodecUtil;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import sagex.miniclient.util.VerboseLogging;
+
+import java.util.List;
+
+public class CustomMediaCodecSelector implements MediaCodecSelector
+{
+    protected final Logger log = LoggerFactory.getLogger(this.getClass());
+    
+
+    
+    @Override
+    public List<MediaCodecInfo> getDecoderInfos(String mimeType, boolean requiresSecureDecoder, boolean requiresTunnelingDecoder) throws MediaCodecUtil.DecoderQueryException
+    {
+        //List<MediaCodecInfo> codecs = MediaCodecSelector.DEFAULT.getDecoderInfos(mimeType, requiresSecureDecoder);
+        List<MediaCodecInfo> codecs =  MediaCodecSelector.DEFAULT.getDecoderInfos(mimeType, requiresSecureDecoder, requiresTunnelingDecoder);
+            
+                log.debug("JVL - getDecoderInfos called MimeType={} ", mimeType);
+        for(int i = 0; i < codecs.size(); i++)
+        {
+            log.debug("JVL - DecoderIndex {}, Decoder Name = {}", i, codecs.get(i).name);
+        }
+    
+        return codecs;
+    }
+    
+    @Nullable
+    @Override
+    public MediaCodecInfo getPassthroughDecoderInfo() throws MediaCodecUtil.DecoderQueryException
+    {
+        MediaCodecInfo codec = MediaCodecSelector.DEFAULT.getPassthroughDecoderInfo();
+    
+        log.debug("JVL - getPassthroughDecoderInfo called Decoder Name={} ", codec.name);
+        
+        return codec;
+    }
+}
---
+++ android-shared/src/main/java/sagex/miniclient/android/video/exoplayer2/CustomRenderersFactory.java
@@ -0,0 +1,51 @@
+package sagex.miniclient.android.video.exoplayer2;
+
+import android.content.Context;
+import android.os.Handler;
+import android.support.annotation.Nullable;
+
+import com.google.android.exoplayer2.DefaultRenderersFactory;
+import com.google.android.exoplayer2.Renderer;
+import com.google.android.exoplayer2.audio.AudioRendererEventListener;
+import com.google.android.exoplayer2.drm.DrmSessionManager;
+import com.google.android.exoplayer2.drm.FrameworkMediaCrypto;
+import com.google.android.exoplayer2.metadata.MetadataOutput;
+import com.google.android.exoplayer2.text.TextOutput;
+import com.google.android.exoplayer2.video.VideoRendererEventListener;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CustomRenderersFactory extends DefaultRenderersFactory
+{
+    protected final Logger log = LoggerFactory.getLogger(this.getClass());
+
+    public CustomRenderersFactory(Context context)
+    {
+        super(context);
+        //super.buildAudioRenderers();s();
+
+    }
+
+    @Override
+    public Renderer[] createRenderers(Handler eventHandler,
+                                      VideoRendererEventListener videoRendererEventListener,
+                                      AudioRendererEventListener audioRendererEventListener,
+                                      TextOutput textRendererOutput,
+                                      MetadataOutput metadataRendererOutput,
+                                      @Nullable DrmSessionManager<FrameworkMediaCrypto> drmSessionManager)
+    {
+
+        Renderer [] renderers = super.createRenderers(eventHandler,videoRendererEventListener,audioRendererEventListener,textRendererOutput,metadataRendererOutput,drmSessionManager);
+
+        for(int i = 0; i < renderers.length; i++)
+        {
+            //log.debug("JVL - Renderer -> {} {}", i , renderers[i].toString());
+        }
+
+        return renderers;
+    }
+
+
+
+}
